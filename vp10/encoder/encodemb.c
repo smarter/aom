2d801eb054163cccd55e8b882dd3bbc44cf6eccf
Yushin Cho <ycho@mozilla.com>
2016-02-16 16:16:29 -0800

    fix compile warnings.
    Change-Id: Ifdea6dcb33054c86585896ea60b5e94b35472ef8

--- vp10/encoder/encodemb.c
+++ vp10/encoder/encodemb.c
@@ -1301,7 +1301,8 @@ void vp10_encode_block_intra(int plane, int block, int blk_row, int blk_col,
   // existing VP10 transform functions
 
   // transform block size in pixels
-  int tx_blk_size = 1 << (tx_size + 2);
+  int tx_blk_size;
+  tx_blk_size = 1 << (tx_size + 2);
   int i, j;
   for (j=0; j < tx_blk_size; j++)
     for (i=0; i < tx_blk_size; i++) {
@@ -1317,7 +1318,7 @@ void vp10_encode_block_intra(int plane, int block, int blk_row, int blk_col,
         // For this, forward transform is applied to 1) predicted image
         // and 2) target original image.
         // Note that pvq in decoder also need to apply forward transform
-        // to predicted image.
+        // to predicted image to obtain reference vector.
 
         //forward transform of predicted image.
         fwd_txfm_32x32(x->use_lp32x32fdct, pred, pvq_ref_coeff, tx_blk_size,
