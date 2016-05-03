a6a1f90308c5eba8731b85cd8e1743aef379eb2f
Yushin Cho <ycho@mozilla.com>
2016-05-03 14:55:23 -0700

    clean up comments.
    Change-Id: Ie01f289130a8e87d102e5d23349a195056d91482

--- vp10/encoder/pvq_encoder.c
+++ vp10/encoder/pvq_encoder.c
@@ -791,8 +791,6 @@ int od_pvq_encode(daala_enc_ctx *enc,
   else {
     dc_rate = -OD_LOG2((double)(skip_cdf[3] - skip_cdf[2])/
      (double)(skip_cdf[2] - skip_cdf[1]));
-    // TODO: Enable this later, since its change in out[0] causes DC change
-    // and thus quality degrade in aom codebase.
     out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate);
   }
   tell = od_ec_enc_tell_frac(&enc->ec);
