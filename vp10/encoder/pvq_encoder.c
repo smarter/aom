13daa6f7696f3af1c4da0a16a006ccb8bd86cc0a
Guillaume Martres <gmartres@mozilla.com>
2016-06-15 16:25:14 +0100

    opt1 patch 6
    Change-Id: I2d017fbbbe5d1fb237ffc365696afcc72063aaba

--- vp10/encoder/pvq_encoder.c
+++ vp10/encoder/pvq_encoder.c
@@ -411,14 +411,16 @@ static int pvq_theta(od_coeff *out, const od_coeff *x0, const od_coeff *r0,
   if (n <= OD_MAX_PVQ_SIZE && !od_vector_is_null(r0, n) && corr > 0) {
     od_val16 xr[MAXN];
     int gain_bound;
+    int upgain;
     gain_bound = OD_SHR(cg - gain_offset, OD_CGAIN_SHIFT);
+    upgain = OD_SHR(cg - gain_offset + (OD_CGAIN_SHIFT<<1>>1), OD_CGAIN_SHIFT);
     /* Perform theta search only if prediction is useful. */
     theta = OD_ROUND32(OD_THETA_SCALE*acos(corr));
     m = od_compute_householder(r16, n, gr, &s, rshift);
     od_apply_householder(xr, x16, r16, n);
     for (i = m; i < n - 1; i++) xr[i] = xr[i + 1];
     /* Search for the best gain within a reasonable range. */
-    for (i = OD_MAXI(1, gain_bound - 1 + speed); i <= gain_bound + 1; i++) {
+    for (i = OD_MAXI(1, gain_bound - 1 + speed); i <= upgain; i++) {
       int j;
       od_val32 qcg;
       int ts;
@@ -429,7 +431,7 @@ static int pvq_theta(od_coeff *out, const od_coeff *x0, const od_coeff *r0,
       /* Search for the best angle within a reasonable range. */
       for (j = OD_MAXI(0, (int)floor(.5 + theta*OD_THETA_SCALE_1*2/M_PI*ts)
        - 2 + speed); j <= OD_MINI(ts - 1,
-       (int)ceil(theta*OD_THETA_SCALE_1*2/M_PI*ts)); j++) {
+       (int)ceil(theta*OD_THETA_SCALE_1*2/M_PI*ts - .5)); j++) {
         double cos_dist;
         double cost;
         double dist_theta;
