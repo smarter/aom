6fd7aeaeb2e156f916d9335348b1c3c0efe815b3
Yushin Cho <ycho@mozilla.com>
2016-06-24 13:36:04 -0700

    more inter mode fix in encode_block() when ac and/or dc is decided as skippable by ME.
    Change-Id: I29ab00918db7a08c98d89701a06c88d895a20622

--- vp10/encoder/encodemb.c
+++ vp10/encoder/encodemb.c
@@ -1212,6 +1212,9 @@ static void encode_block(int plane, int block, int blk_row, int blk_col,
       if (x->skip_txfm[0] == SKIP_TXFM_AC_DC && plane == 0) {
         // skip forward transform
         p->eobs[block] = 0;
+#if CONFIG_PVQ
+        pvq_info->ac_dc_coded = 0;
+#endif
         *a = *l = 0;
         return;
       } else {
@@ -1232,6 +1235,9 @@ static void encode_block(int plane, int block, int blk_row, int blk_col,
         } else {
           // skip forward transform
           p->eobs[block] = 0;
+#if CONFIG_PVQ
+          pvq_info->ac_dc_coded = 0;
+#endif
           *a = *l = 0;
           return;
         }
