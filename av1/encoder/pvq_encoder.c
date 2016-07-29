b4f4b656d6324055949dad9dce4023b153d333a8
Guillaume Martres <gmartres@mozilla.com>
2016-07-28 21:54:46 -0700

    DC skip rdo fixes
    Change-Id: I5cbc8649f69bed8e6fa73eedf248fcf59ffdabb2

--- av1/encoder/pvq_encoder.c
+++ av1/encoder/pvq_encoder.c
@@ -613,11 +613,11 @@ static void pvq_encode_partition(od_ec_enc *ec,
  * @retval quantized value
  */
 int od_rdo_quant(od_coeff x, int q, double delta0, double pvq_norm_lambda) {
-  int threshold;
+  int n;
   /* Optimal quantization threshold is 1/2 + lambda*delta_rate/2. See
      Jmspeex' Journal of Dubious Theoretical Results for details. */
-  threshold = 128 + OD_CLAMPI(0, (int)(256*pvq_norm_lambda*delta0/2), 128);
-  if (abs(x) < q*threshold/256) {
+  n = OD_DIV_R0(abs(x), q);
+  if ((double)abs(x)/q < (double)n/2 + pvq_norm_lambda*delta0/(2*n)) {
     return 0;
   }
   else {
@@ -760,10 +760,29 @@ int od_pvq_encode(daala_enc_ctx *enc,
   od_encode_checkpoint(enc, &buf);
   if (is_keyframe) out[0] = 0;
   else {
-    dc_rate = -OD_LOG2((double)(skip_cdf[3] - skip_cdf[2])/
-     (double)(skip_cdf[2] - skip_cdf[1]));
-    out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
-     enc->pvq_norm_lambda_dc);
+    int n;
+    n = OD_DIV_R0(abs(in[0] - ref[0]), dc_quant);
+    if (n == 0) {
+      out[0] = 0;
+    } else {
+      int tell2;
+      od_rollback_buffer dc_buf;
+
+      dc_rate = -OD_LOG2((double)(skip_cdf[3] - skip_cdf[2])/
+       (double)(skip_cdf[2] - skip_cdf[1]));
+      dc_rate += 1;
+
+      tell2 = od_ec_enc_tell_frac(&enc->ec);
+      od_encode_checkpoint(enc, &dc_buf);
+      generic_encode(&enc->ec, &enc->state.adapt.model_dc[pli],
+       n - 1, -1, &enc->state.adapt.ex_dc[pli][bs][0], 2);
+      tell2 = od_ec_enc_tell_frac(&enc->ec) - tell2;
+      dc_rate += tell2/8.0;
+      od_encode_rollback(enc, &dc_buf);
+
+      out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
+       enc->pvq_norm_lambda);
+    }
   }
   tell = od_ec_enc_tell_frac(&enc->ec);
   /* Code as if we're not skipping. */
@@ -840,11 +859,30 @@ int od_pvq_encode(daala_enc_ctx *enc,
   if (nb_bands == 0 || skip_diff <= enc->pvq_norm_lambda/8*tell) {
     if (is_keyframe) out[0] = 0;
     else {
-      dc_rate = -OD_LOG2((double)(skip_cdf[1] - skip_cdf[0])/
-       (double)skip_cdf[0]);
-      out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
-       enc->pvq_norm_lambda_dc);
-    }
+      int n;
+      n = OD_DIV_R0(abs(in[0] - ref[0]), dc_quant);
+      if (n == 0) {
+        out[0] = 0;
+      } else {
+        int tell2;
+        od_rollback_buffer dc_buf;
+
+        dc_rate = -OD_LOG2((double)(skip_cdf[1] - skip_cdf[0])/
+         (double)skip_cdf[0]);
+        dc_rate += 1;
+
+        tell2 = od_ec_enc_tell_frac(&enc->ec);
+        od_encode_checkpoint(enc, &dc_buf);
+        generic_encode(&enc->ec, &enc->state.adapt.model_dc[pli],
+         n - 1, -1, &enc->state.adapt.ex_dc[pli][bs][0], 2);
+        tell2 = od_ec_enc_tell_frac(&enc->ec) - tell2;
+        dc_rate += tell2/8.0;
+        od_encode_rollback(enc, &dc_buf);
+
+        out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
+         enc->pvq_norm_lambda);
+      }
+   }
     /* We decide to skip, roll back everything as it was before. */
     od_encode_rollback(enc, &buf);
     od_encode_cdf_adapt(&enc->ec, out[0] != 0, skip_cdf,
